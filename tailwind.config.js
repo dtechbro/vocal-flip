584c2cacd3bd33a2e0958396f61bf78cfa0828d2
dtechbro <princeakinola05@gmail.com>
2023-08-31 22:05:01 +0100

    mini landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6366F1'
+      }
+    },
   },
   plugins: [],
 }
