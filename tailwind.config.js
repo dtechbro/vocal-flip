b2353efa88a3ddff6bf96dcd5f783cd221ef3521
dtechbro <princeakinola05@gmail.com>
2024-04-12 10:05:48 +0100

    edited config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./build/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
